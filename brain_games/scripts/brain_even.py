030374c48a3db8e267731a9d7cb79bdcf9b28827
Sergei Dmitriev <sergei.dmitriev@vk.team>
2023-06-20 17:54:40 +0300

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,47 +1,10 @@
 #!/usr/bin/env python3
 
-import prompt
-import random
-import sys
-
-from brain_games.game_engine import welcome_user
-
-
-def is_even(number):
-    return number % 2 == 0
+from brain_games.games.even import game
 
 
 def main():
-    username = welcome_user()
-    questions = []
-    questions_num = 3
-    max_random_number = 100
-    random.seed()
-    for _ in range(questions_num):
-        questions.append(random.randrange(max_random_number))
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for question in questions:
-        print("Question:", question)
-        answer = prompt.string("Your answer: ")
-        if is_even(question):
-            if answer == "yes":
-                print("Correct!")
-                continue
-            else:
-                print(f"'{answer}' is wrong answer ;(. \
-                      Correct answer was 'yes'")
-                print(f"Let's try again, {username}!")
-                sys.exit(0)
-        else:
-            if answer == "no":
-                print("Correct!")
-                continue
-            else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
-                print(f"Let's try again, {username}!")
-                sys.exit(0)
-
-    print(f"Contgratulations, {username}!")
+    game()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,6 +6,5 @@ from brain_games.game_engine import welcome_user
 def main():
     welcome_user()
 
-
 if __name__ == '__main__':
     main()
